e1ddf901a12666cfc9c4ae5330267d2a9e687644
fistadev <fabiofistarol@gmail.com>
2021-12-13 21:40:00 +0100

    Update models.py

--- upload/models.py
+++ upload/models.py
@@ -1,11 +1,23 @@
 from django.db import models
 
+from django.urls import reverse
+
+
 
 class Upload(models.Model):
-    file = models.FileField(upload_to='uploads/')
-    uploaded_at = models.DateTimeField(auto_now_add=True)
+    file = models.FileField(upload_to='csv/')
+    upload_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.file.name
+
+    def get_absolute_url(self):
+        return reverse('upload-detail', args=[str(self.id)])
+
+
 
 
 
 # Django request/response cycle
+# VMT: View, Model, Template
 # URL -> View -> Model -> Template -> HttpResponse
