05f1271ffd4569b73b294e8d4e02ff9db544917c
fistadev <fabiofistarol@gmail.com>
2021-12-13 21:39:49 +0100

    Update admin.py

--- upload/admin.py
+++ upload/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.db import models
 
-# Register your models here.
+from .models import Upload
+
+
+class UploadAdmin(admin.ModelAdmin):
+    model = Upload
+    list_display = ('file', 'upload_date')
+
+
+
+admin.site.register(Upload, UploadAdmin)
