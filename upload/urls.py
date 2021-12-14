ab9c29f9ccf572ebc4ea23fb294c24c4aa04a8c2
fistadev <fabiofistarol@gmail.com>
2021-12-14 15:25:23 +0100

    Update urls.py

--- upload/urls.py
+++ upload/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.HomeView.as_view(), name='home'),
-    path(r'^media/csv/$', views.upload_csv, name='upload_csv'),
+    # path('', views.home, name='home'),
+    path('results/', views.results, name='results'),
+    path('', views.HomeView.as_view(), name='home_list'),
+    # path(r'^media/csv/$', views.upload_csv, name='upload_csv'),
 ]
