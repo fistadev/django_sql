0b27bbb14363bb54019c7cd768c332b760ca0361
fistadev <fabiofistarol@gmail.com>
2021-12-15 09:23:29 +0100

    Update models.py

--- upload/models.py
+++ upload/models.py
@@ -3,10 +3,26 @@ from django.db import models
 from django.urls import reverse
 
 
-class Books(models.Model):
-    title = models.CharField(("title"), max_length=200)
-    author = models.CharField(("author"), max_length=200)
-    genres = models.TextField(("genres"), max_length=500)
+
+# class Books(models.Model):
+#     title = models.CharField(("title"), max_length=200)
+#     author = models.CharField(("author"), max_length=200)
+#     genres = models.TextField(("genres"), max_length=500)
+
+#     def __str__(self):
+#         return self.title
+
+
+class BooksComplete(models.Model):
+    title = models.CharField(max_length=50)
+    author = models.CharField(max_length=50)
+    num_rating = models.IntegerField()
+    num_reviews = models.IntegerField()
+    avg_rating = models.FloatField()
+    num_pages = models.IntegerField()
+    original_publish_year = models.IntegerField()
+    award = models.CharField(max_length=50)
+
 
     def __str__(self):
         return self.title
@@ -23,18 +39,6 @@ class Books(models.Model):
 #         return reverse('upload-detail', args=[str(self.id)])
 
 
-# class BooksComplete(models.Model):
-#     title = models.CharField(max_length=50)
-#     author = models.CharField(max_length=50)
-#     num_rating = models.IntegerField()
-#     num_reviews = models.IntegerField()
-#     avg_rating,num_pages = models.FloatField(),models.IntegerField()
-#     original_publish_year = models.IntegerField()
-#     series = models.CharField(max_length=50)
-#     minmax_norm_ratings = models.FloatField(),models.FloatField()
-#     mean_norm_ratings = models.FloatField()
-#     genres = models.CharField(max_length=50)
-#     award = models.CharField(max_length=50)
 
 
 # Django request/response cycle
