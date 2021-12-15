991aefd33cfcc3c7a22aa310b9f996a33faf5eca
fistadev <fabiofistarol@gmail.com>
2021-12-15 09:23:25 +0100

    Update admin.py

--- upload/admin.py
+++ upload/admin.py
@@ -4,11 +4,19 @@ from django.contrib.admin.decorators import register
 from .models import *
 
 
-class BooksAdmin(admin.ModelAdmin):
-    model = Books
-    list_display = ('title',)
-    list_filter = ('genres',)
+# class BooksAdmin(admin.ModelAdmin):
+#     model = Books
+#     list_display = ('title',)
+#     list_filter = ('genres',)
 
 
-admin.site.register(Books, BooksAdmin)
+class CompleteBooksAdmin(admin.ModelAdmin):
+    model = BooksComplete
+    list_display = ('title', 'author', 'original_publish_year', 'avg_rating')
+    list_filter = ('original_publish_year', )
+
+
+# admin.site.register(Books, BooksAdmin)
+admin.site.register(BooksComplete, CompleteBooksAdmin)
 # admin.site.register(Upload)
+
